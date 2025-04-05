feccf5aa3bf22ff3064cc5a35d7e3d9925daf5b9
FelipeMaD <felipemarinhodr@hotmail.com>
2025-04-05 12:00:08 -0300

    criando rotas públicas + privadas para a aplicação

--- projuridico_frontend/src/App.jsx
+++ projuridico_frontend/src/App.jsx
@@ -1,13 +1,75 @@
 
 import react from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+// importando os componentes referentes às páginas =-=-=-=-
+
+import Login from "./pages/Login"
+import Cadastro from "./pages/Cadastro"
+import Erro404 from "./pages/Erro404"
+import Home from "./pages/Home"
+import Comunidade from "./pages/Comunidade"
+
+// importando os componentes das nossas rotas protegidas =-=-=-=-
+
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/login"/>
+}
+
+function CadastroELogout(){
+  localStorage.clear()
+  return <Cadastro />
+}
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        <Route 
+          path="/" 
+          element={
+            <Home/>
+          } 
+        />
+
+        <Route
+          path="/login"
+          element={
+            <Login/>
+          }
+        />
+
+        <Route 
+          path="/cadastro"
+          element={
+            <CadastroELogout />
+          }
+        />
+
+        <Route 
+          path="*"
+          element = {
+            <Erro404 />
+          }
+        />
+
+        <Route 
+          path="/comunidade" 
+          element={
+            <ProtectedRoute>
+              <Comunidade />
+            </ProtectedRoute>
+          } 
+        />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- projuridico_frontend/src/api.js
+++ projuridico_frontend/src/api.js
@@ -6,7 +6,7 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
 
-api.interceptors.request.user((config) =>{
+api.interceptors.request.use((config) =>{
     const token = localStorage.getItem(ACCESS_TOKEN);
 
     if (token){
