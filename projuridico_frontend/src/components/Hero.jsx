5de537039ebb320158c70f88e1cbe80da003b3a2
Lucas Wanderley Gomes <lucas.wanderley.gomes@gmail.com>
2025-04-10 13:03:12 -0300

    Correção do componente hero

--- projuridico_frontend/src/components/Hero.jsx
+++ projuridico_frontend/src/components/Hero.jsx
@@ -1,20 +1,41 @@
 import "../styles/components/Hero.css"
+import "../styles/assents/hero.png"
 
 const Hero = () => {
   return (
-    <section className="hero">
-      <div className="divider"></div>
-      
-      <div className="content">
-        <h3 className="name">Danielle Lucena</h3>
-        <h2 className="specyalits">Advogada Especializada</h2>
+    <section class="main">
+        <div class="hero">
+            <header>
+                <h2>Projuridico</h2>
+
+                <div class="items">
+                    <a href="">Advocacia</a>
+                    <a href="">Consultoria</a>
+                    <a href="">Contato</a>
+                    <a href="">Comunidade</a>
+                </div>
+            </header>
+
+            <div class="action">
+                <div class="name">
+                    <p> Danielle Lucena</p>
+                    <h4>Advocacia Especializada</h4>
+                </div>
+
+                <img src="hero.png" alt=""></img>
+            </div>
+        </div>
         
-        <ul className="features">
-          <li>Apresentação de projetos e investimentos, defensores do direito da mulher</li>
-          <li>Comunidade de interesses coletivos referentes aos movimentos empreendedores femininos</li>
-          <li>Planejamento das providências jurídicas a serem adotadas, minimizando riscos e custos</li>
-        </ul>
-      </div>
+        <div class="call">
+            <button class="login">Login</button>
+
+            <div class ="more">
+             <h3>Nossa missão é contribuir para o fortalecimento da autonomia das mulheres </h3>
+             <p>A partir de uma assessoria jurídica humanizada.</p>
+             <button>Saiba mais</button>
+            </div>
+
+        </div>
     </section>
   );
 };
