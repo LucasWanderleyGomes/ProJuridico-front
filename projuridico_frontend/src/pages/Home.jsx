60de58f81731232bed9961be509487e9c6476565
Lucas Wanderley Gomes <lucas.wanderley.gomes@gmail.com>
2025-04-14 20:26:29 -0300

    Comunidade componente do home

--- projuridico_frontend/src/pages/Home.jsx
+++ projuridico_frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Community from "../components/Community";
 import Contact from "../components/Contact";
