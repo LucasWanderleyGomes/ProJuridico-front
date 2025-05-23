7a6e304bc81b9a6aca31b05cf4dcee3faad9db13
FelipeMaD <felipemarinhodr@hotmail.com>
2025-05-23 16:52:26 -0300

    [PORTFOLIO - ADVOCACIA] - refatorando ideias
    estilizando componentes

--- projuridico_frontend/src/pages/Portfolio.jsx
+++ projuridico_frontend/src/pages/Portfolio.jsx
@@ -7,6 +7,13 @@ import { useState, useEffect } from 'react'
 import PageDecoration from '../components/PageDecoration'
 import bg from "../assets/bgSection.png"
 import {motion} from "framer-motion"
+
+// ASSETS
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
+//STYLES
+
+//COMPONENTS
 import imageContent from "../assets/dani.png"
 
 import bgPorftolio from "../assets/bg-portfolio.png"
@@ -20,9 +27,12 @@ const Portfolio = () => {
   const [categoria, setCategoria] = useState("")
   const [titulo, setTitulo] = useState("")
   const [descricao, setDescricao] = useState("")
+  const [escondidos, setEscondidos] = useState([]);
+  
 
   useEffect(() => {
-      getProcesso()
+      getProcesso();
+
   }, [] )
 
   const getProcesso = () =>{
@@ -31,19 +41,33 @@ const Portfolio = () => {
       .then((data) => {setPortfolio(data.results) ; console.log(data.results)})
       .catch((err) => alert(err));
   }
+  
+  const handleDeleteProcesso = (id) => {
+  api.delete(`/api/v2/processos/${id}/`)
+    .then((res) => {
+      if (res.status === 204) {
+        
+        const modalEl = document.getElementById(`modal-${id}`);
+        const modalInstance = bootstrap.Modal.getInstance(modalEl);
+        modalInstance.hide();
 
-  const handleDeleteProcesso = (id) =>{
-      api.delete(`/api/v2/processos/${id}/`)
-      .then((res) => {
-        if (res.status === 204) {
-          alert(`Processo ${res.data.id} deletado com sucesso!`)
-        } else {
-          alert("Erro ao deletar o processo selecionado.")
-        }
-        getProcesso();
-      }).catch((error) => alert(error));
-      
-  }
+        alert(`Processo ${id} deletado com sucesso!`);
+      } else {
+        alert("Erro ao deletar o processo selecionado.");
+      }
+      getProcesso();
+    })
+    .catch((error) => alert(error));
+};
+
+const handleEsconderProcesso = (id) => {
+  setEscondidos((prev) => [...prev, id]);
+
+
+  const modalEl = document.getElementById(`modal-${id}`);
+  const modalInstance = bootstrap.Modal.getInstance(modalEl);
+  modalInstance.hide();
+};
 
   const handleCriarProcesso = (e) =>{
     e.preventDefault();
@@ -61,6 +85,7 @@ const Portfolio = () => {
   }
 
 
+
   return (
     <>
       <Header/>
@@ -68,7 +93,7 @@ const Portfolio = () => {
      
       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner" id='carrossel-bg'>
-          <div class="carousel-item active" data-bs-interval="4000" id='item-carrossel1'>
+          <div class="carousel-item active" data-bs-interval="6000" id='item-carrossel1'>
             <motion.div
               initial={{ opacity: 0, y: 50 }}
               whileInView={{ opacity: 1, y: 0 }}
@@ -82,7 +107,12 @@ const Portfolio = () => {
                     <img src={imageContent} alt="" id='img-dani' />
                 </div>
                 <div className="left-container-info">
+                  
+                  <h4 className='titulo-cars-port'>Base de atuação</h4>
                   <p>Nosso ponto de partida e principal base de atuação é a defesa das mulheres no âmbito das estruturas familiares, que são os espaços onde as diferenças – essências distintas - podem, primeiramente, se transformar em desigualdades – privilégios de uns em detrimento de outros - e se transmutar em violências.</p>
+                  <div className='div-numero-slide'>
+                      <p className='id-slider-port'>1</p>
+                  </div>
                 </div>
                 
               </div>
@@ -91,7 +121,7 @@ const Portfolio = () => {
             </motion.div>
 
           </div>
-          <div class="carousel-item" data-bs-interval="4000" id='item-carrossel2'>
+          <div class="carousel-item" data-bs-interval="6000" id='item-carrossel2'>
             <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
@@ -105,7 +135,12 @@ const Portfolio = () => {
                     <img src={imageContent} alt="" id='img-dani' />
                   </div>
                   <div className="left-container-info">
+                   
+                    <h4 className='titulo-cars-port'> Proteção</h4>
                     <p>E, em reforço, faz-se necessário impulsionar outras formas de emancipação da mulher. Daí nasce a necessidade de ampliar a atuação para a proteção da sua produção intelectual e de sua base empresarial, sobretudo na vertente educacional; do seu acesso à saúde, aos benefícios previdenciários e à possibilidade de transcendência dos frutos do seu trabalho e interesses pessoais, a partir do planejamento sucessório.</p>
+                    <div className='div-numero-slide'>
+                      <p className='id-slider-port'>2</p>
+                    </div>
                   </div>
                 
                 </div>
@@ -113,7 +148,7 @@ const Portfolio = () => {
             </motion.div>
             
           </div>
-          <div class="carousel-item" data-bs-interval="4000" id='item-carrossel3'>
+          <div class="carousel-item" data-bs-interval="6000" id='item-carrossel3'>
             <motion.div
                   initial={{ opacity: 0, y: 50 }}
                   whileInView={{ opacity: 1, y: 0 }}
@@ -135,7 +170,13 @@ const Portfolio = () => {
                           <img src={imageContent} alt="" id='img-dani' />
                         </div>
                         <div className="left-container-info">
+                          
+                          <h4 className='titulo-cars-port'>Responsáveis</h4>
                           <p>A atuação é coordenada e executada em maior parte pela Dra. @daniellelucenaadv , mas também conta com a colaboração e alta competência técnica de outras advogadas, formando uma grande rede de mulheres que defendem mulheres e vivem juntas um potente processo de empoderamento, em vias individual e coletiva, como deve ser.</p>
+                          <div className='div-numero-slide'>
+                            <p className='id-slider-port'>3</p>
+                          </div>
+                          
                         </div>
                       
                       </div>
@@ -153,11 +194,11 @@ const Portfolio = () => {
           </div>
         </div>
         <button id='previous-but' class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <GrPrevious className='icones-carr'/>
           <span class="visually-hidden">Previous</span>
         </button>
         <button id='next-but' class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <GrNext className='icones-carr'/>
           <span class="visually-hidden">Next</span>
         </button>
       </div>
@@ -175,7 +216,17 @@ const Portfolio = () => {
         <section className='section-portfolio' style={{ backgroundImage: `url(${bgPorftolio})`}} >
           <h2 id='title-section-processos'>Destaques de processos</h2>
           <p id='texto-sec-proc'>“ O direito é um poder passivo ou pacificado pelo Estado e é sinônimo de poder, pois sem esta participação e legitimação democrática, só resta a violência, a descrença e a barbárie.” (Hannah Arendt – Filósofa )</p>
-          {portfolio.map((processo) => <Processo processo={processo} onDelete={handleDeleteProcesso} key={processo.id} /> )}
+          {portfolio
+            .filter((processo) => !escondidos.includes(processo.id))
+            .map((processo) => (
+              <Processo
+                processo={processo}
+                onDelete={handleDeleteProcesso}
+                onEsconder={handleEsconderProcesso}
+                key={processo.id}
+                
+              />
+          ))}
         </section>
       </motion.div>
       
